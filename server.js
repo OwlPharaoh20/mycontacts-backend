b9a77882bf4aed9be7c7fbd3538116b51489878b
GG-Pharaoh <oxdiablo20@gmail.com>
2024-09-14 06:43:13 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//Create Express server
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.listen(port, ()=> {
+    console.log(`Server is running on port ${port}`)
+})
