87f282d4ed054781ecbd75506f2780cf0a4505d7
GG-Pharaoh <oxdiablo20@gmail.com>
2024-09-15 01:17:53 -0700

    Current state

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 //Create Express server
 const express = require("express");
+const dotenv = require("dotenv").config();
+
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT ||5000;
+
+
+//add middleware 
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
 
 app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}`)
+    console.log(`Server is running on port ${port}`);
 })
